e512eb945910034adb82d14ed60198409a5a07fb
AdrianoCng <adrianocangiamila@gmail.com>
2020-07-30 02:45:01 +0100

    update

--- php/database.php
+++ php/database.php
@@ -5,16 +5,7 @@
 // $dbPassword = "";
 // $dbName = "geonames";
 
-// $dbServername = "eu-cdbr-west-03.cleardb.net";
-// $dbUsername = "bf818bc3326135";
-// $dbPassword = "e16fd17d ";
-// $dbName = "heroku_7f07fc2c6e7a68c";
-
 // $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-
-// mysql://bf818bc3326135:e16fd17d@eu-cdbr-west-03.cleardb.net/heroku_7f07fc2c6e7a68c?reconnect=true
-
-// $url=parse_url(getenv("mysql://bf818bc3326135:e16fd17d@eu-cdbr-west-03.cleardb.net/heroku_7f07fc2c6e7a68c?reconnect=true"));
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
