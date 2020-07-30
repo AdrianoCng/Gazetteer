4a8e708bb146a1206d3b9d5d2b25b3adae139658
AdrianoCng <adrianocangiamila@gmail.com>
2020-07-30 02:00:37 +0100

    update

--- php/database.php
+++ php/database.php
@@ -1,8 +1,15 @@
 <?php
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "geonames";
+// $dbServername = "localhost";
+// $dbUsername = "root";
+// $dbPassword = "";
+// $dbName = "geonames";
+
+$dbServername = "eu-cdbr-west-03.cleardb.net";
+$dbUsername = "bf818bc3326135";
+$dbPassword = "e16fd17d ";
+$dbName = "heroku_7f07fc2c6e7a68c";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+// mysql://bf818bc3326135:e16fd17d@eu-cdbr-west-03.cleardb.net/heroku_7f07fc2c6e7a68c?reconnect=true
