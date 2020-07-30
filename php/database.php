115b5778f20c810e60fc2316db81eea8b7e7524c
AdrianoCng <adrianocangiamila@gmail.com>
2020-07-30 02:23:41 +0100

    update

--- php/database.php
+++ php/database.php
@@ -5,11 +5,22 @@
 // $dbPassword = "";
 // $dbName = "geonames";
 
-$dbServername = "eu-cdbr-west-03.cleardb.net";
-$dbUsername = "bf818bc3326135";
-$dbPassword = "e16fd17d ";
-$dbName = "heroku_7f07fc2c6e7a68c";
+// $dbServername = "eu-cdbr-west-03.cleardb.net";
+// $dbUsername = "bf818bc3326135";
+// $dbPassword = "e16fd17d ";
+// $dbName = "heroku_7f07fc2c6e7a68c";
 
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+// $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
 // mysql://bf818bc3326135:e16fd17d@eu-cdbr-west-03.cleardb.net/heroku_7f07fc2c6e7a68c?reconnect=true
+
+$url=parse_url(getenv("mysql://bf818bc3326135:e16fd17d@eu-cdbr-west-03.cleardb.net/heroku_7f07fc2c6e7a68c?reconnect=true"));
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"],1);
+
+  mysql_connect($server, $username, $password);
+
+  mysql_select_db($db);
